452d2a55960edf1658e733524ab30cad12f1c5a6
sommyajitdutta <sommyajit1999@gmail.com>
2024-09-28 22:23:37 +0530

    ✨ New Project

---
+++ src/main/java/com/scaler/msproject/MsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.msproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/msproject/MsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.msproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
